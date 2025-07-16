a03f935e6a3da025f85b30991a918e01694f3590
Luís Gustavo <luisgustavo.teodoro52@gmail.com>
2025-07-16 00:59:43 -0300

    build: iniciar o projeto com as configurações iniciais e arquivos do emerson

---
+++ src/main/java/br/edu/ifsuldeminas/mch/retromaster/RetromasterApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsuldeminas.mch.retromaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetromasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetromasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsuldeminas/mch/retromaster/RetromasterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsuldeminas.mch.retromaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetromasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
